a942f3dd593c15d9ac033d89bca9ab567faf523b
jaye-jaye <823701573@qq.com>
2023-02-19 05:48:34 +0800

    update start_admin.sh

--- start_admin.sh
+++ start_admin.sh
@@ -1,1 +1,4 @@
 kubeadm init --kubernetes-version 'v1.23.4' --pod-network-cidr=10.244.0.0/16
+# setup network, refs: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#pod-network
+# https://github.com/flannel-io/flannel#deploying-flannel-manually
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
